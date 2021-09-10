7eaad24eac42a6c3c7190ab0ddf6d2c3680895c1
Willian Augusto Sonsin <willian.augustos7@gmail.com>
2021-09-09 23:35:53 -0300

    statics charts

--- projeto-sds3/frontend/src/App.tsx
+++ projeto-sds3/frontend/src/App.tsx
@@ -1,12 +1,28 @@
 import NavBar from "components/NavBar";
 import Footer from "components/Footer";
 import DataTable from "components/DataTable";
+import BarCharts from "components/BarCharts";
+import DonutChart from "components/DonutChart"
 function App() {
   return (
     <>
       <NavBar />
       <div className="container">
-        <h5 className="text-primary"> olá mundo</h5>
+        <h2 className="text-primary py-3 text-center"> DashBoard de vendas</h2>
+        <div className="row px-3">
+          
+          <div className="col-sm-6">
+            <h5 className="text-secondary text-center">taxa de sucesso(%)</h5>
+            <BarCharts />
+          </div>
+
+          <div className="col-sm-6">
+            <h5 className="text-secondary text-center">taxa de sucesso(%)</h5>
+            <DonutChart />
+          </div>
+
+        </div>
+        <h2 className="text-primary py-3 text-center"> Vendas</h2>
         <DataTable />
       </div>
       <Footer />
